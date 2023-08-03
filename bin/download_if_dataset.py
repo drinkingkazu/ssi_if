e8260c27725c03ed66729bd9c3b20943316a990e
drinkingkazu <kterao@slac.stanford.edu>
2023-08-03 15:16:29 -0700

    bug fix

--- bin/download_if_dataset.py
+++ bin/download_if_dataset.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python3
 import sys
 
-'''
-image   test  https://drive.google.com/file/d/1L2yjBkzL3Ruaf8HuaMDR5PC9nETG5t_i/view?usp=drive_link
-image   train https://drive.google.com/file/d/130Lm_4K2cCclnmOEZlVIBKKPkJUn3k3f/view?usp=drive_link
-segment test  https://drive.google.com/file/d/1Q-86MIsKf7ZQxE-9_vZKVPM7FKMI15L6/view?usp=drive_link
-segment train https://drive.google.com/file/d/1YOmIt3hMc3vViH5oZ8TRnQUBrBW7rocW/view?usp=drive_link
-graph   test  https://drive.google.com/file/d/16j9m8kc6gXs_3qbB-Ta2xEPTwdQTh0r3/view?usp=drive_link
-graph   train https://drive.google.com/file/d/1iqF9IhdQS9ICKpkjnoFgTIMwa-ULtSGx/view?usp=drive_link
-'''
+#'''
+#image   test  https://drive.google.com/file/d/1L2yjBkzL3Ruaf8HuaMDR5PC9nETG5t_i/view?usp=drive_link
+#image   train https://drive.google.com/file/d/130Lm_4K2cCclnmOEZlVIBKKPkJUn3k3f/view?usp=drive_link
+#segment test  https://drive.google.com/file/d/1Q-86MIsKf7ZQxE-9_vZKVPM7FKMI15L6/view?usp=drive_link
+#segment train https://drive.google.com/file/d/1YOmIt3hMc3vViH5oZ8TRnQUBrBW7rocW/view?usp=drive_link
+#graph   test  https://drive.google.com/file/d/16j9m8kc6gXs_3qbB-Ta2xEPTwdQTh0r3/view?usp=drive_link
+#graph   train https://drive.google.com/file/d/1iqF9IhdQS9ICKpkjnoFgTIMwa-ULtSGx/view?usp=drive_link
+#'''
 
 
 DATA_KEYS=dict(image   = dict(train='130Lm_4K2cCclnmOEZlVIBKKPkJUn3k3f',test='1L2yjBkzL3Ruaf8HuaMDR5PC9nETG5t_i'),
